75971f50a2fdc79fda01e11a06ef5a4350515eb4
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-10-19 00:44:01 +0200

    server connected with clound based monogodb database

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 // middleware for application
 app.use(express.json());
 app.use(cors());
+const uri = `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.qlijsqf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const productsCollection = client.db("productsDB").collection("products");
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // root route for testing purposes
-app.get('/',(req, res) =>{
-    res.send('hello world');
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
